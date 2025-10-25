9f5ca8620cb0063ea6486720f34668881c0cffcd
Luis-Dev <luis.ljate@gmail.com>
2025-10-25 17:47:11 +0200

    Create ClassChangeManager.java

---
+++ Elytria Essentials/src/main/java/me/luisgamedev/elytriaEssentials/ClassLimiter/ClassChangeManager.java
@@ -0,0 +1,16 @@
+package me.luisgamedev.elytriaEssentials.ClassLimiter;
+
+import net.Indyuce.mmocore.api.event.PlayerChangeClassEvent;
+import org.bukkit.event.EventHandler;
+
+public class ClassChangeManager {
+
+    public ClassChangeManager() {
+
+    }
+
+    @EventHandler
+    public static void onClassChange(PlayerChangeClassEvent event) {
+        
+    }
+}
