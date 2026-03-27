d17f0516842878197d3910bbf4648cd771b9d672
Luis-Dev <luis.ljate@gmail.com>
2026-03-27 19:28:37 +0100

    Send coalmine sentence explanation on offline login

--- Elytria Essentials/src/main/java/me/luisgamedev/elytriaEssentials/Coalmine/CoalmineManager.java
+++ Elytria Essentials/src/main/java/me/luisgamedev/elytriaEssentials/Coalmine/CoalmineManager.java
@@ -76,10 +76,7 @@ public class CoalmineManager implements Listener {
         if (grantPickaxe) {
             giveUnbreakablePickaxe(player);
         }
-        player.sendMessage(color("&7You have been sent to the coalmine. Remaining coal blocks: &f" + amount));
-        player.sendMessage(color("&7Due to your misbehaviour, you have been sent to the Coalmine of Elytria."));
-        player.sendMessage(color("&7Mine &f" + amount + " &7coal blocks to earn your freedom."));
-        player.sendMessage(color("&7Turn them in to &fFrank&7, and he will let you leave once you have delivered them all."));
+        sendSentenceExplanation(player, amount);
     }
 
     public boolean releasePlayer(Player player, boolean clearInventory) {
@@ -105,6 +102,8 @@ public class CoalmineManager implements Listener {
             teleportToCoalmine(player);
             ensurePickaxe(player);
             applyMiningFatigueIfInCoalmine(player);
+            repository.getRemainingBlocks(player.getUniqueId())
+                    .ifPresent(remaining -> sendSentenceExplanation(player, remaining));
         });
     }
 
@@ -233,6 +232,14 @@ public class CoalmineManager implements Listener {
                 false,
                 false
         ));
+    }
+
+
+    private void sendSentenceExplanation(Player player, int remainingBlocks) {
+        player.sendMessage(color("&7You have been sent to the coalmine. Remaining coal blocks: &f" + remainingBlocks));
+        player.sendMessage(color("&7Due to your misbehaviour, you have been sent to the Coalmine of Elytria."));
+        player.sendMessage(color("&7Mine &f" + remainingBlocks + " &7coal blocks to earn your freedom."));
+        player.sendMessage(color("&7Turn them in to &fFrank&7, and he will let you leave once you have delivered them all."));
     }
 
     private int removeCoalBlocks(PlayerInventory inventory) {
